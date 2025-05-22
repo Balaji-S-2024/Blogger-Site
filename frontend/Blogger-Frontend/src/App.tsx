01c8f3b5c81f3c410740f13877c2f72e6c43253c
Balaji <balajisankar0202@gmail.com>
2025-05-22 22:28:09 +0530

    path correction

--- frontend/Blogger-Frontend/src/App.tsx
+++ frontend/Blogger-Frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css';
 // import React from 'react'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import LandingPage from './components/pages/home/LandingPage';
+import LandingPage from './components/pages/landing-page/LandingPage';
 import SignIn from './components/pages/signin/SignIn';
 import SignUp from './components/pages/siginup/SignUp';
 import Home from './components/pages/home/Home';
